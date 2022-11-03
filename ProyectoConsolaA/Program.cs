4f935aef06b99a38dad4e5fb40bca8cf7370ce12
Hernando Tejada <htejadag25@gmail.com>
2022-11-03 18:23:54 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoConsolaA/Program.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//import 
+//include
+
+using System;
+
+namespace ProyectoConsolaA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var hola = args;
+            //Console.WriteLine("Hello World!");
+            //System.out.println("Hola mundo!"),
+            string a = Console.ReadLine();
+            Console.WriteLine("Hola " + a);
+            // Hola 
+
+        }
+    }
+}

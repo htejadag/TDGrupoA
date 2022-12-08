9afc6b179d2e839d81ba46e697ddf93c007e8ca3
Hernando Tejada <htejadag25@gmail.com>
2022-12-08 18:19:08 -0500

    Ejemplo de manejo de excepciones

--- ProyectoConsolaA/Program.cs
+++ ProyectoConsolaA/Program.cs
@@ -10,14 +10,57 @@ namespace ProyectoConsolaA
     {
         static void Main(string[] args)
         {
-            //var hola = args;
-            //Console.WriteLine("Hello World!");
-            //System.out.println("Hola mundo!"),
-            string a = Console.ReadLine();
-            Console.WriteLine("Hola " + a);
-            // Hola
-            // Hola 2
-            //hola 3 
+            Convertir();
+            //Ingresar();
+        }
+
+        public static void Convertir()
+        {
+
+            try
+            {
+                int num = 0;
+                Console.WriteLine("Ingrese un numero : ");
+                string dato = Console.ReadLine();
+                
+                bool resultado = int.TryParse(dato, out num);
+                
+                Console.WriteLine("Se pudo convertir : " + resultado);
+                Console.WriteLine("El numero es : "+ num);
+            }            
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public static void Ingresar() {           
+            
+            try
+            {
+                Int16 edad = 0;
+                Console.WriteLine("Ingrese su edad: ");
+                string dato = Console.ReadLine(); 
+                edad = Int16.Parse(dato); 
+                Console.WriteLine("Tienes " + edad + " años"); 
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+                // Almacenarse en un log de errores
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
 
         }
     }

bd2259f1c0ad7abad5048258b107526d4497b05b
Hernando Tejada <htejadag25@gmail.com>
2022-12-15 18:21:28 -0500

    cambio efectuadoImportar Libreria Serilog

--- ProyectoConsolaA/PruebaLog.cs
+++ ProyectoConsolaA/PruebaLog.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Serilog;
 
 namespace ProyectoConsolaA
 {
@@ -12,11 +13,40 @@ namespace ProyectoConsolaA
 
         // Métodos
 
-        public void IngresaNombre() {
+        public void IngresaNombre()
+        {
 
             Console.WriteLine("Ingrese su nombre :");
             string nombre = Console.ReadLine();
             Console.WriteLine("Hola :" + nombre);
+        }
+
+        public void DividirEntreCero(int a, int b)
+        {
+            try
+            {
+                Log.Debug("");
+
+                Console.WriteLine("El valor de a es : " + a);
+                Console.WriteLine("El valor de b es : " + b);
+
+                int c;
+                //Console.WriteLine("Ingrese un número:");
+                //string aa = Console.ReadLine();
+                //a = int.Parse(aa);
+
+                //Console.WriteLine("Ingrese otro número:");
+                //string bb = Console.ReadLine();
+                //b = int.Parse(bb);
+
+                c = a / b;
+
+                Console.WriteLine("El resultado es :" + c);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
     }

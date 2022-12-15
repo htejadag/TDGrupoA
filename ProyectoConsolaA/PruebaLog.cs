df8066d9684cfb2233231336a4331910c6e3bfe1
Hernando Tejada <htejadag25@gmail.com>
2022-12-15 17:45:42 -0500

    Crear clase PruebaLog

---
+++ ProyectoConsolaA/PruebaLog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoConsolaA
+{
+    public class PruebaLog
+    {
+
+        // Atributos 
+
+
+        // Métodos
+
+        public void IngresaNombre() {
+
+            Console.WriteLine("Ingrese su nombre :");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hola :" + nombre);
+        }
+
+    }
+}

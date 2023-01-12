4329e911cea414e6d36a2c0bae5d682fb7e6aa4e
Hernando Tejada <htejadag25@gmail.com>
2023-01-12 18:26:19 -0500

    Creación de Data Annotation en la clase Producto

--- TDA.Ms.Producto.API/Producto.cs
+++ TDA.Ms.Producto.API/Producto.cs
@@ -1,10 +1,18 @@
-﻿namespace TDA.Ms.Producto.API
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TDA.Ms.Producto.API
 {
     public class Producto
     {
         public int IdProducto { get; set; }
+
+        [Required]
         public string Nombre { get; set; }
+
+        [Required]
         public decimal Precio { get; set; }
+
+        [Required]
         public int Cantidad { get; set; }
     }
 }

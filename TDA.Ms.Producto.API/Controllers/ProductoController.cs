52178273080e4dcc2c052f2999de23c19d6bd8e9
Hernando Tejada <htejadag25@gmail.com>
2023-01-12 17:39:52 -0500

    Implementar método Get en el controlador

---
+++ TDA.Ms.Producto.API/Controllers/ProductoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace TDA.Ms.Producto.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductoController
+    {
+        public static List<Producto> listaProducto = new List<Producto>
+            {
+                new Producto()
+                {
+                    IdProducto = 1,
+                    Nombre = "Arroz",
+                    Precio = 10.0m,
+                    Cantidad = 5
+                },
+                new Producto()
+                {
+                    IdProducto = 2,
+                    Nombre = "Leche",
+                    Precio = 8.0m,
+                    Cantidad = 11
+                }
+            };
+
+        [HttpGet(Name = "ListarProductos")]
+        public IEnumerable<Producto> ListarProductos()
+        {            
+            return listaProducto;
+        }
+
+
+    }
+}

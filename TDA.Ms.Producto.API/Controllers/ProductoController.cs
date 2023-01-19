6b7f36fead9e8bbe33665b46ce2d8b94764cb507
Hernando Tejada <htejadag25@gmail.com>
2023-01-19 17:08:39 -0500

    ListarProductos apuntando a base de datos

--- TDA.Ms.Producto.API/Controllers/ProductoController.cs
+++ TDA.Ms.Producto.API/Controllers/ProductoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System;
+using MongoDB.Driver;
 
 namespace TDA.Ms.Producto.API.Controllers
 {
@@ -9,52 +10,58 @@ namespace TDA.Ms.Producto.API.Controllers
     [Route("[controller]")]
     public class ProductoController : ControllerBase
     {
-        public static List<Producto> listaProducto = new List<Producto>
-            {
-                new Producto()
-                {
-                    IdProducto = 1,
-                    Nombre = "Arroz",
-                    Precio = 10.0m,
-                    Cantidad = 5
-                },
-                new Producto()
-                {
-                    IdProducto = 2,
-                    Nombre = "Leche",
-                    Precio = 8.0m,
-                    Cantidad = 11
-                }
-            };
+        //public static List<Producto> listaProducto = new List<Producto>
+        //    {
+        //        new Producto()
+        //        {
+        //            IdProducto = 1,
+        //            Nombre = "Arroz",
+        //            Precio = 10.0m,
+        //            Cantidad = 5
+        //        },
+        //        new Producto()
+        //        {
+        //            IdProducto = 2,
+        //            Nombre = "Leche",
+        //            Precio = 8.0m,
+        //            Cantidad = 11
+        //        }
+        //    };
 
         [HttpGet(Name = "ListarProductos")]
         public IEnumerable<Producto> ListarProductos()
         {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("TDA_productos");
+            var collection = database.GetCollection<Producto>("producto");
+
+            var listaProducto = collection.Find(x=>true).ToList();
+
             return listaProducto;
         }
 
         [HttpPost(Name = "CrearProducto")]
         public ActionResult<Producto> CrearProducto(Producto producto)
         {
-            producto.IdProducto = listaProducto.Count() + 1;
-            listaProducto.Add(producto);
+            //producto.IdProducto = listaProducto.Count() + 1;
+            //listaProducto.Add(producto);
             return CreatedAtAction("CrearProducto", producto);
         }
 
         [HttpPut(Name = "ModificarProducto")]
         public ActionResult<Producto> ModificarProducto(Producto producto)
         {
-            var productoModificado = listaProducto.Single(x => x.IdProducto == producto.IdProducto);
-            productoModificado.Nombre = producto.Nombre;
-            productoModificado.Precio = producto.Precio;
-            productoModificado.Cantidad = producto.Cantidad;
+            //var productoModificado = listaProducto.Single(x => x.IdProducto == producto.IdProducto);
+            //productoModificado.Nombre = producto.Nombre;
+            //productoModificado.Precio = producto.Precio;
+            //productoModificado.Cantidad = producto.Cantidad;
             return CreatedAtAction("ModificarProducto", producto);
         }
 
         [HttpDelete(Name = "EliminarProducto")]
         public ActionResult<Producto> EliminarProducto(int idProducto)
         {
-            listaProducto.RemoveAt(idProducto - 1);
+            //listaProducto.RemoveAt(idProducto - 1);
             return CreatedAtAction("EliminarProducto", idProducto);
         }
 
--- TDA.Ms.Producto.API/Producto.cs
+++ TDA.Ms.Producto.API/Producto.cs
@@ -1,18 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace TDA.Ms.Producto.API
 {
     public class Producto
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+
+        
         public int IdProducto { get; set; }
 
-        [Required]
+
         public string Nombre { get; set; }
 
-        [Required]
+
         public decimal Precio { get; set; }
 
-        [Required]
+
         public int Cantidad { get; set; }
     }
 }

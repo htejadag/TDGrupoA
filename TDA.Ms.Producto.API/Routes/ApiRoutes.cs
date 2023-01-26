ba1b50621e9570a88f5753ea5e6e3fd306d43b9d
Hernando Tejada <htejadag25@gmail.com>
2023-01-26 18:17:08 -0500

    cambiar cadena de conexión

--- TDA.Ms.Producto.API/Routes/ApiRoutes.cs
+++ TDA.Ms.Producto.API/Routes/ApiRoutes.cs
@@ -16,19 +16,6 @@
             public const string Create = Base + "/producto/create";
             public const string Update = Base + "/producto/update";
             public const string Delete = Base + "/producto/delete";
-        }
-
-        public static class RouteCategorias
-        {
-            // Read
-            public const string GetAll = Base + "/categoria/all";
-            public const string GetById = Base + "/categoria/{id}";
-
-            // Write
-            public const string Create = Base + "/categoria/create";
-            public const string Update = Base + "/categoria/update";
-            public const string Delete = Base + "/categoria/delete";
-
-        }
+        }        
     }
 }
